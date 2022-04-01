3eb652d8b3fd145d2ca1c13bcb2d806e83057b2f
serega-s <comp.it.05@inbox.ru>
2022-04-01 20:34:48 +0300

    added models Book and UserBookRelation

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,6 +6,22 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.id}: {self.name}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = [
+        (1, 'I hated it'),
+        (2, 'I didnt like it'),
+        (3, 'it was ok'),
+        (4, 'I liked it'),
+        (5, 'I loved it'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
