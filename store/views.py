74743fa7430a40ef13b051b8f1b1bc8305bad751
serega-s <comp.it.05@inbox.ru>
2022-04-01 20:35:09 +0300

    CBV

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BooksSerializer
 
 
@@ -12,11 +13,15 @@ class BooksViewSet(ModelViewSet):
     serializer_class = BooksSerializer
     queryset = Book.objects.all()
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
 
+    def perform_create(self, serializer):
+        # serializer.validated_data['owner'] = self.request.user
+        serializer.save(owner=self.request.user)
 
-def auth(request):
-    return render(request, 'oauth.html')
+
+class AuthView(TemplateView):
+    template_name = 'oauth.html'
